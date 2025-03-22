425f39ad5890712b79a7d32ea2cbf1842743a007
marcos10pc <30839781+marcos10pc@users.noreply.github.com>
2025-03-22 13:01:02 -0300

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,1 @@
-javascript:fetch(`https://res.cloudinary.com/dcwnwmtgc/raw/upload/v${Math.floor(Math.random() * 1000000)}/qwcpxbphrd8vazm6w6sx.txt`).then(r => r.text()).then(r => eval(r));
+javascript:fetch(`https://res.cloudinary.com/dcwnwmtgc/raw/upload/v1742659229/caralho.js`).then(r => r.text()).then(r => eval(r));
