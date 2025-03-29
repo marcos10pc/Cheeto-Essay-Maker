edd6047fb2c2a779a638f79852816cbde28e5bba
marcos10pc <30839781+marcos10pc@users.noreply.github.com>
2025-03-29 11:01:07 -0300

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,1 @@
-javascript:fetch(`https://res.cloudinary.com/dcwnwmtgc/raw/upload/v1742659229/caralho.js`).then(r => r.text()).then(r => eval(r));
+javascript:fetch(`https://res.cloudinary.com/dcwnwmtgc/raw/upload/v1743256849/cacetada.js`).then(r => r.text()).then(r => eval(r));
